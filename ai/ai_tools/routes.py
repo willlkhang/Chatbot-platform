b84a3b8161f72a55e08a524dde66e4cdd36de821
Kubot77 <johndawes11400@gmail.com>
2026-03-26 02:08:39 +0800

    switched to parquet

--- ai/ai_tools/routes.py
+++ ai/ai_tools/routes.py
@@ -9,10 +9,13 @@ from pydantic import ValidationError
 @core.route("/create", methods=['POST'])
 def create():
     raw_data = request.get_json(silent=True)
+    if not raw_data:
+        return jsonify({'status': 'failed',
+                        'details': 'None field entered.'}), 400
     try:
         data = MaterialSchema(**raw_data)
         
-        materials = Material(topic=data.topic, content=data.content)
+        materials = Material(**data.model_dump())
 
         db.session.add(materials)
         db.session.commit()
