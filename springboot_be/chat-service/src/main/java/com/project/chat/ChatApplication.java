37153e259f8bc0638e5c875606837511724d6bcd
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-25 20:29:00 +0800

    Chat: init new service, this service manages chat features, with functionalities for chat history, delete, and get

---
+++ springboot_be/chat-service/src/main/java/com/project/chat/ChatApplication.java
@@ -0,0 +1,12 @@
+package com.project.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+public class ChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+}
