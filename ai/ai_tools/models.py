3664d04536803cad481885bab68f57d980e96bdf
Kubot77 <johndawes11400@gmail.com>
2026-03-26 21:47:49 +0800

    routes added, pray

--- ai/ai_tools/models.py
+++ ai/ai_tools/models.py
@@ -4,4 +4,6 @@ class Material(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     topic = db.Column(db.Integer, nullable=False)
     content = db.Column(db.String, nullable=False)
-    
+
+    def __repr__(self):
+        return f"<Material {self.id} - Topic {self.topic}>"
