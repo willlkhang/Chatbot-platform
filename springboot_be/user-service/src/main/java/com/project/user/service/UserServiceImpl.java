6c75c9d3ed8125154e8331e7286dae64f567b32a
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-27 00:41:03 +0800

    user-service: modify handler to deal with DTO and mapper

--- springboot_be/user-service/src/main/java/com/project/user/service/UserServiceImpl.java
+++ springboot_be/user-service/src/main/java/com/project/user/service/UserServiceImpl.java
@@ -2,31 +2,35 @@ package com.project.user.service;
 
 import com.project.base.domain.User;
 import com.project.base.outputDto.UserResponse;
+import com.project.user.mapper.UserMapper;
 import com.project.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public List<UserResponse> getAllUsers() {
         List<User> users = userRepository.getAllUser();
-        return null;
+        return users.stream().map(a -> userMapper.toResponse(a)).collect(Collectors.toList());
     }
 
     @Override
     public User getUserById(Long id) {
-        userRepository.findUserByUserId(id);
+        return userRepository.findUserByUserId(id);
     }
 
     @Override
     public User getUserByUsername(String name) {
-        userRepository.getUserByUsername(name);
+        return userRepository.getUserByUsername(name);
     }
 }
