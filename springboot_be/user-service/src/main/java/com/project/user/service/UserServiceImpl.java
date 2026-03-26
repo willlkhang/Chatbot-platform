14b694fe523fa09a4666ce141675ff45b95be99c
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-27 00:35:01 +0800

    User-service: mapper hides username and password

--- springboot_be/user-service/src/main/java/com/project/user/service/UserServiceImpl.java
+++ springboot_be/user-service/src/main/java/com/project/user/service/UserServiceImpl.java
@@ -2,22 +2,31 @@ package com.project.user.service;
 
 import com.project.base.domain.User;
 import com.project.base.outputDto.UserResponse;
+import com.project.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public List<UserResponse> getAllUsers() {
-        return List.of();
+        List<User> users = userRepository.getAllUser();
+        return null;
     }
 
     @Override
-    public User getUserById(String id) {
-        return null;
+    public User getUserById(Long id) {
+        userRepository.findUserByUserId(id);
     }
 
     @Override
     public User getUserByUsername(String name) {
-        return null;
+        userRepository.getUserByUsername(name);
     }
 }
