6880a9df6a1142771f8c1c89ccf1fa73dda1d52f
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-26 23:58:44 +0800

    chat: base controller

--- springboot_be/chat-service/src/main/java/com/project/chat/controller/ChatController.java
+++ springboot_be/chat-service/src/main/java/com/project/chat/controller/ChatController.java
@@ -1,16 +1,43 @@
 package com.project.chat.controller;
 
+import com.project.chat.domain.Chat;
 import com.project.chat.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ChatController {
 
     @Autowired
     ChatService chatService;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> getChatByUserId(@PathVariable Long userId){
+        Chat chat = chatService.getChatByUserId(userId);
+        if(chat == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(chat);
+    }
+
+    @PostMapping("/user/{id}")
+    public ResponseEntity<?> getChatById(@PathVariable Long id){
+        Chat chat = chatService.getChatById(id);
+        if(chat == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(chat);
+    }
+
+    @PostMapping("/user/{chatId}/chat/{userId}")
+    public ResponseEntity<?> updateConversation(@RequestParam Long chatId,
+                                                @RequestParam Long userId,
+                                                @RequestParam List<String> messages){
+        chatService.updateConversation(chatId, userId, messages);
+
+        return ResponseEntity.ok(messages);
+    }
 }
