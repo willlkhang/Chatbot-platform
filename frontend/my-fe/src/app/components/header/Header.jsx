a5b0aeb534dc5558d7f25d65a7e302555ccb3831
willlkhang <minhkhangnguyen31104@gmail.com>
2026-03-27 23:43:45 +0800

    FE: just layout

--- frontend/my-fe/src/app/components/header/Header.jsx
+++ frontend/my-fe/src/app/components/header/Header.jsx
@@ -1,18 +1,23 @@
 "use client";
 import { useEffect, useState } from "react";
 
-import { LuBrainCircuit } from "react-icons/lu"; 
-
 const Header = () =>{
     return (
         <div className="header">
-            <div className="header-left">
+            <div className="header-top">
                 <div className="logo-group">
-                    <div className="logo">
-                        <LuBrainCircuit className='icon'/>;
+                    <div className="block-center">
+                        <div className="logo">
+                            <a href="">
+                                <img src="/logo/logo.png" alt="/error/error.png" />
+                            </a>
+                        </div>
+                        <p className="slogan">Ask Me Anything in your ICT283</p>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+
+export default Header;
