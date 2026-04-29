26473439245d7b3b696c725e54589a8b12650b6b
willlkhang <minhkhangnguyen31104@gmail.com>
2026-04-29 18:13:23 +0800

    backend: user service: make bean for passwords encoder

--- springboot_be/user-service/src/main/java/com/project/user/UserApplication.java
+++ springboot_be/user-service/src/main/java/com/project/user/UserApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.project.base.domain")
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- springboot_be/user-service/src/main/java/com/project/user/controller/UserController.java
+++ springboot_be/user-service/src/main/java/com/project/user/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.project.user.controller;
 
+// user domain group
 import com.project.base.domain.User;
 import com.project.user.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RestController
 @RequestMapping("/api/user")
@@ -14,6 +19,9 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public ResponseEntity<?> createUser(@RequestBody User user){
         userService.createUser(user);
