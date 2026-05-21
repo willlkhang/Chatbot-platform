1ec48695e481c41cfca9c579a8a69d87db31e502
willlkhang <minhkhangnguyen31104@gmail.com>
2026-05-22 00:21:27 +0800

    rag: clean the code from node in langgraph

--- ragbot/node.py
+++ ragbot/node.py
@@ -1,18 +1,24 @@
 from dotenv import load_dotenv
-from langgraph.graph import MessagesState
+from langgraph.graph import MessagesState, END
 from langgraph.prebuilt import ToolNode
-from langchain_core.messages import HumanMessage, SystemMessage
-from langgraph.graph import START, MessagesState, StateGraph, END
+from langchain_core.messages import SystemMessage
 
 from react import tools, llm_with_tools
-from prompts.system_prompts import system__prompt
+from prompts.system_prompts import system_prompt
 
 load_dotenv()
 
-SYSTEM_MSG = SystemMessage(content=system__prompt)
+SYSTEM_MSG = SystemMessage(content=system_prompt)
+
 
 class AgentNodes:
-    def __init__(self, *, system_message: SystemMessage | None = None, llm_with_tools_instance=None, tools_list=None) -> None:
+    def __init__(
+        self,
+        *,
+        system_message: SystemMessage | None = None,
+        llm_with_tools_instance=None,
+        tools_list=None,
+    ) -> None:
         self.system_message = system_message or SYSTEM_MSG
         self.llm_with_tools = llm_with_tools_instance or llm_with_tools
         self.tools = tools_list or tools
@@ -27,12 +33,14 @@ class AgentNodes:
             return END
         return "tools"
 
+
+_nodes = AgentNodes()
+tool_node = _nodes.tool_node
+
+
 def run_agent_reasoning(state: MessagesState):
     return _nodes.run_agent_reasoning(state)
 
+
 def should_continue(state: MessagesState):
     return _nodes.should_continue(state)
-
-_nodes = AgentNodes()
-
-tool_node = _nodes.tool_node
